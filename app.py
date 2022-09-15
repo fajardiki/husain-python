bdc18d429b6f30fadddcc8984abaa6838068db83
Fajar Diki <fajarsiddiqi@gmail.com>
2022-09-15 11:16:47 +0700

    Fajar, upload 11:16

--- app.py
+++ app.py
@@ -3,13 +3,14 @@ import pandas as pd
 
 data_container = []
 
-for i, tweet in enumerate(nstwitter.TwitterSearchScraper('#esport').get_items()) :
-    if i > 100 :
+for i, tweet in enumerate(nstwitter.TwitterSearchScraper('esport indonesia lang:in').get_items()) :
+    if i > 5000 :
         break
-    data_container.append([tweet.date, tweet.rawContent, tweet.likeCount, tweet.quoteCount])
+    data_container.append([tweet.rawContent, tweet.user, tweet.hashtags, tweet.lang, tweet.date])
 
-data_pd = pd.DataFrame(data_container, columns=["Date Tweet", "Tweet", "Count of Likes", "Count of quote"])
-data_pd['Date Tweet'] = pd.to_datetime(data_pd['Date Tweet'])
+data_pd = pd.DataFrame(data_container, columns=["Tweet", "User", "Hastag", "Bahasa", "Tanggal"])
+data_pd['Tanggal'] = data_pd['Tanggal'].apply(lambda a: pd.to_datetime(a).date())
 
 # print(data_pd.to_excel(r'C:\Users\ASUS\Documents\export_data_tweet.xlsx', ))
-data_pd.to_excel(r'C:\Users\ASUS\Documents\export_data_tweet.xlsx', index=False, header=True)
+# data_pd.to_excel(r'C:\Users\ASUS\Documents\export_data_tweet.xlsx', index=False, header=True)
+data_pd.to_excel(r'D:\FREELANCE\husain-python\export_data_tweet.xlsx', index=False, header=True)
