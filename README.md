6439d82727d2f7be3730da0981c622f71e19c544
Fajar Diki <fajarsiddiqi@gmail.com>
2022-09-23 11:31:56 +0700

    Fajar, preprocessing

--- README.md
+++ README.md
@@ -1,3 +1,34 @@
+## PREPROCESING DATA ------------------------------------------------------------------
+- dalam procesing data saya mengikuti tutorial https://yunusmuhammad007.medium.com/text-preprocessing-menggunakan-pandas-nltk-dan-sastrawi-untuk-large-dataset-5fb3c0a88571
+- menggunakan library The Natural Language Toolkit (NLTK) https://pypi.org/project/nltk/
+
+# fajar 2022-09-23
+## CLEANSING
+- proses menghilangkan / menghapus komponen dari  tweet yang tidak memiliki pengaruh apapun  pada analisis sentimen
+
+# fajar 2022-09-23
+## CASE FOLDING
+- proses merubah huruf kapital menjadi huruf kecil dengan tujuan agar semua huruf seragam
+- menggunakan pandas, df['Tweet'] = df['Tweet'].str.lower()
+
+## SCRAPING DATA ----------------------------------------------------------------------
+
+# fajar 2022-09-23
+## proses fetch data / wraping data 
+- wrapping data menggunakan library snscrapper
+- link gitHub https://github.com/JustAnotherArchivist/snscrape
+- kenapa menggunakan snscrapper ?
+- karena penggunaan nya mudah dan tidak perlu apiKey, dan tentu snscrape library legal
+- tapi documentasi dari library snscape ini kurang menurut saya
+- karena hanya ada gitHubnya saja, jadi jika ingin tau cara pakainya bisa cari di pencarian google karena banyak juga yang menyediakan 
+- tutorial cara penggunaannya. Atua kalo mau bisa langsung baca kodingnya di gitHub
+- meski begitu gitHubnya masih aktif dan terakhir di perbaruai di bulan january 2022
+- star gitHubnya 1.6K
+- alasan itu menurut saya sudah cukup untuk saya memilih library ini
+
+# fajar 2022-09-23
+## ini adalah project TA freelance
+- Judul dari TA ini adalah Analisis Sentiment E-sport di indonesian
+
 # husain-python
 freelance
-
--- preprocesing.py
+++ preprocesing.py
@@ -1,3 +1,68 @@
 import pandas as pd
 
-pd.read_excel('D:\FREELANCE\husain-python\')
+import string
+import re
+
+from nltk.tokenize import word_tokenize
+from nltk.probability import FreqDist
+
+# import nltk
+# nltk.download('punkt')
+
+data = pd.read_excel('export_data_tweet_all.xlsx')
+df = pd.DataFrame(data).head(10)
+
+# CASE FOLDING
+df['Tweet'] = df['Tweet'].str.lower()
+
+# TOKENIZING
+def remove_tweet_special(text):
+    # remove tab, new line, ans back slice
+    text = text.replace('\\t'," ").replace('\\n'," ").replace('\\u'," ").replace('\\',"")
+    # remove non ASCII (emoticon, chinese word, .etc)
+    text = text.encode('ascii', 'replace').decode('ascii')
+    # remove mention, link, hashtag
+    text = ' '.join(re.sub("([@#][A-Za-z0-9]+)|(\w+:\/\/\S+)"," ", text).split())
+    # remove incomplete URL
+    return text.replace("http://", " ").replace("https://", " ")
+
+df['Tweet'] = df['Tweet'].apply(remove_tweet_special)
+
+#remove number
+def remove_number(text):
+    return  re.sub(r"\d+", "", text)
+
+df['Tweet'] = df['Tweet'].apply(remove_number)
+
+#remove punctuation
+def remove_punctuation(text):
+    return text.translate(str.maketrans("","",string.punctuation))
+
+df['Tweet'] = df['Tweet'].apply(remove_punctuation)
+
+#remove whitespace leading & trailing
+def remove_whitespace_LT(text):
+    return text.strip()
+
+df['Tweet'] = df['Tweet'].apply(remove_whitespace_LT)
+
+#remove multiple whitespace into single whitespace
+def remove_whitespace_multiple(text):
+    return re.sub('\s+',' ',text)
+
+df['Tweet'] = df['Tweet'].apply(remove_whitespace_multiple)
+
+# remove single char
+def remove_singl_char(text):
+    return re.sub(r"\b[a-zA-Z]\b", "", text)
+
+df['Tweet'] = df['Tweet'].apply(remove_singl_char)
+
+# NLTK word Tokenize 
+def word_tokenize_wrapper(text):
+    return word_tokenize(text)
+
+df['Tweet Tokens'] = df['Tweet'].apply(word_tokenize_wrapper)
+
+# df.to_excel('data_tokenizing_tes.xlsx')
+print(df)
