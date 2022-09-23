0ead20122490ddaed0385096ae2232f033174dd9
Fajar Diki <fajarsiddiqi@gmail.com>
2022-09-23 08:23:18 +0700

    Fajar, masuk ke proses preprocessing data

---
+++ Catatan.md
@@ -0,0 +1,4 @@
+# fajar 2022-09-15
+## Pengambilan data tanpa limit  
+- dimulai jam 11:33
+- selesai jam 11:40
--- app.py
+++ app.py
@@ -4,13 +4,16 @@ import pandas as pd
 data_container = []
 
 for i, tweet in enumerate(nstwitter.TwitterSearchScraper('esport indonesia lang:in').get_items()) :
-    if i > 5000 :
-        break
-    data_container.append([tweet.rawContent, tweet.user, tweet.hashtags, tweet.lang, tweet.date])
+    # if i > 5000 :
+    #     break
+    data_container.append([tweet.rawContent])
+    # data_container.append([tweet.rawContent, tweet.user, tweet.hashtags, tweet.lang, tweet.date])
 
-data_pd = pd.DataFrame(data_container, columns=["Tweet", "User", "Hastag", "Bahasa", "Tanggal"])
-data_pd['Tanggal'] = data_pd['Tanggal'].apply(lambda a: pd.to_datetime(a).date())
+data_pd = pd.DataFrame(data_container, columns=["Tweet"])
+# data_pd = pd.DataFrame(data_container, columns=["Tweet", "User", "Hastag", "Bahasa", "Tanggal"])
+# data_pd['Tanggal'] = data_pd['Tanggal'].apply(lambda a: pd.to_datetime(a).date())
 
 # print(data_pd.to_excel(r'C:\Users\ASUS\Documents\export_data_tweet.xlsx', ))
 # data_pd.to_excel(r'C:\Users\ASUS\Documents\export_data_tweet.xlsx', index=False, header=True)
-data_pd.to_excel(r'D:\FREELANCE\husain-python\export_data_tweet.xlsx', index=False, header=True)
+# data_pd.to_excel(r'D:\FREELANCE\husain-python\export_data_tweet_all.xlsx', index=False, header=True)
+data_pd.to_csv(r'D:\FREELANCE\husain-python\export_data_tweet_all.xlsx', index=False, header=True)
